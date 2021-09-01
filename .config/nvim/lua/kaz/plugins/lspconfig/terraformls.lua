1f301117f5602bfd163304f70b60d86099c250c0
Kevin Zhuang <kevin7441@gmail.com>
2021-09-01 06:51:18 +0000

    nvim: terraformls performance

--- .config/nvim/lua/kaz/plugins/lspconfig/terraformls.lua
+++ .config/nvim/lua/kaz/plugins/lspconfig/terraformls.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup(on_attach, capabilities)
   require('lspconfig').terraformls.setup({
     on_attach = on_attach,
+    flags = { debounce_text_changes = 150 },
     capabilities = capabilities,
   })
 end
--- .config/nvim/lua/kaz/plugins/lspconfig/tflint.lua
+++ .config/nvim/lua/kaz/plugins/lspconfig/tflint.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup(on_attach)
   require('lspconfig').tflint.setup({
     on_attach = on_attach,
+    flags = { debounce_text_changes = 150 },
   })
 end
 
