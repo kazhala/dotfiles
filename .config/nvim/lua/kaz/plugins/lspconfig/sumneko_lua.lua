41f20391928436d52609d986e22ebbdcab452411
Kevin Zhuang <kevin7441@gmail.com>
2022-03-07 16:13:58 +1100

    nvim: update lua language server location

--- .config/nvim/lua/kaz/plugins/lspconfig/sumneko_lua.lua
+++ .config/nvim/lua/kaz/plugins/lspconfig/sumneko_lua.lua
@@ -2,19 +2,8 @@ local util = require('lspconfig').util
 
 local M = {}
 
-local system_name
-if vim.fn.has('mac') == 1 then
-  system_name = 'macOS'
-elseif vim.fn.has('unix') == 1 then
-  system_name = 'Linux'
-elseif vim.fn.has('win32') == 1 then
-  system_name = 'Windows'
-else
-  print('Unsupported system for sumneko')
-end
-
 local sumneko_root_path = vim.fn.stdpath('data') .. '/lua-language-server'
-local sumneko_binary = sumneko_root_path .. '/bin/' .. system_name .. '/lua-language-server'
+local sumneko_binary = sumneko_root_path .. '/bin/' .. '/lua-language-server'
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
