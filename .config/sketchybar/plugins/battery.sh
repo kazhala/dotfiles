024f86db1d796c9ba2daad6110db986ba32aa73b
Kevin Zhuang <kevin7441@gmail.com>
2026-04-13 20:26:53 +1000

    cleanup

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -3,7 +3,7 @@
 BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 
 if [[ -z "${BATT_PERCENT}" ]]; then
-  sketchybar --set "${NAME}" label="⚡"
+  sketchybar --set "${NAME}" label="󰇄"
 else
   sketchybar --set "${NAME}" label="${BATT_PERCENT}%"
 fi
--- .config/sketchybar/plugins/battery_icon.sh
+++ .config/sketchybar/plugins/battery_icon.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-BATT_PRESENT=$(pmset -g batt | grep -Eo "\d+%")
-
-if [[ -z "${BATT_PRESENT}" ]]; then
-  sketchybar --set "${NAME}" icon="󰚥"
-  exit 0
-fi
-
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
 if [[ ${CHARGING} != "" ]]; then
