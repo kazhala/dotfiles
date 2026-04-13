17e2d86ff2d22cc519d7f3eb7e5240d1e4202484
Kevin Zhuang <kz@canva.com>
2026-04-13 20:23:05 +1000

    fix

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -2,4 +2,8 @@
 
 BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 
-sketchybar --set "${NAME}" label="${BATT_PERCENT}%"
+if [[ -z "${BATT_PERCENT}" ]]; then
+  sketchybar --set "${NAME}" label="⚡"
+else
+  sketchybar --set "${NAME}" label="${BATT_PERCENT}%"
+fi
--- .config/sketchybar/plugins/battery_icon.sh
+++ .config/sketchybar/plugins/battery_icon.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+BATT_PRESENT=$(pmset -g batt | grep -Eo "\d+%")
+
+if [[ -z "${BATT_PRESENT}" ]]; then
+  sketchybar --set "${NAME}" icon="󰚥"
+  exit 0
+fi
+
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
 if [[ ${CHARGING} != "" ]]; then
-	sketchybar --set "${NAME}" icon="󰻹"
-	exit 0
+  sketchybar --set "${NAME}" icon="󰻹"
+  exit 0
 fi
 
 sketchybar --set "${NAME}" icon="󰗶"
